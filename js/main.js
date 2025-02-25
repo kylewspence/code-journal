9adeafe7fd3e6134ff248190e02f78c05512321a
Kyle Spence <kyle.w.spence@gmail.com>
2025-02-25 04:55:23 +0000

    HTML, CSS, First Steps of Jscript

--- js/main.js
+++ js/main.js
@@ -1,12 +1,9 @@
+'use strict';
 // everything else
-
-const $inputUrl = document.querySelector('#photo-url') as HTMLInputElement;
-const $imgPreview = document.querySelector(
-  '.preview-image',
-) as HTMLImageElement;
+const $inputUrl = document.querySelector('#photo-url');
+const $imgPreview = document.querySelector('.preview-image');
 if (!$inputUrl) throw new Error('no input img');
 if (!$imgPreview) throw new Error('no preview');
-
 $inputUrl?.addEventListener('input', () => {
   $imgPreview?.setAttribute('src', $inputUrl.value);
 });
